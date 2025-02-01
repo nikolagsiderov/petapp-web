cd296516db3c01f397e0ff414024a89701d0d30e
nikolagsiderov <nikolagsiderov@gmail.com>
2025-02-01 12:44:37 +0200

    If user is authentication and trying to access the /auth or /register pages, redirect him back to index page

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,8 +1,18 @@
 import MainContainer from "@/app/components/MainContainer";
 import ClientOnly from "@/app/components/ClientOnly";
 import LoginClient from "./LoginClient";
+import { getCurrentUser } from "pawpal-fe-common/users";
+import webTokenGetter from "@/app/context/webTokenGetter";
+import { redirect } from "next/navigation";
 
-const Login = () => {
+const Login = async () => {
+  const response = await getCurrentUser(webTokenGetter());
+  const currentUser = response?.success ? response : null;
+
+  if (response?.success && currentUser) {
+    redirect("/");
+  }
+
   return (
     <ClientOnly>
       <MainContainer>
