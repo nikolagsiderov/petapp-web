0bed604c6d934b591d23ac0a72a7383ecffbb1b5
nikolagsiderov <nikolagsiderov@gmail.com>
2025-03-13 14:34:20 +0200

    Object is possibly undefined, temporary fix

--- app/components/inputs/PasswordInput.tsx
+++ app/components/inputs/PasswordInput.tsx
@@ -59,7 +59,7 @@ const PasswordInput: React.FC<InputProps> = ({
       </label>
       {errors?.[id]?.message && (
         <span className="text-rose-500 text-sm">
-          {t(errors[id].message.toString())}
+          {t(errors[id]?.message?.toString()) ?? ""}
         </span>
       )}
     </div>
